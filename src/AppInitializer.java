e0764e7a021de63a144f9453fb7038b7a37a50f8
NimmiShashikala60 <nimmishashikala385@gmail.com>
2022-07-12 09:25:04 +0530

    new update

--- src/AppInitializer.java
+++ src/AppInitializer.java
@@ -1,2 +1,19 @@
-public class AppInitializer {
+import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Scene;
+import javafx.stage.Stage;
+
+import java.io.IOException;
+
+
+public class AppInitializer extends Application {
+  public static void main(String args[]){
+
+  }
+
+    public void start(Stage primaryStage) throws IOException {
+        primaryStage.setScene(new Scene(FXMLLoader.load(getClass().getResource("/View/LoginDashboardForm.fxml "))));
+        primaryStage.show();
+    }
 }
+
